35f1c85430b328f49da87c0b9dc97cce99335a1d
James Garcia <james.garcia1201@outlook.com>
2022-07-23 18:39:29 -0500

    fixed some code in the Home module for readability

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 import './Home.css';
@@ -7,10 +7,8 @@ const Home = () => {
 
 
      const backgroundVideo: any = require('../../assets/Home/Comp_1_42.mp4');
-
      const [query, setQuery] = useState("");
      const [showMessage, setShowMessage] = useState(false);
-
      const navigate = useNavigate();
 
      useEffect(() => {
@@ -19,7 +17,11 @@ const Home = () => {
 
      const slowVideo = () => {
           const vid: any = document.querySelector('#backgroundVideo');
-          vid.playbackRate = .75;
+          vid.playbackRate = .76;
+          vid.setAttribute('muted', true);
+          vid.setAttribute('playsinline', true);
+          vid.setAttribute('autoplay', true);
+          vid.setAttribute('loop', true);
      }
 
      function handleSearch(): any {
@@ -52,22 +54,25 @@ const Home = () => {
           <div id='homeContainer'>
 
                <div id='videoBox'>
-                    <video src={backgroundVideo} id="backgroundVideo" autoPlay={true} muted={true} loop={true}></video>
+                    <video 
+                         src={backgroundVideo} 
+                         id="backgroundVideo"
+                         autoPlay={true} 
+                         muted={true}
+                         loop={true} 
+                         playsInline={true}
+                         onCanPlay={(e) => {(e.target as HTMLVideoElement).muted=true;}}
+                    >
+                    </video>                 
                </div>
 
                <div id="homeContent">
                     <h1 id='homeTitle'>Sentiment Analysis Powered by Machine Learning</h1>
-
                     <p id="homeSubTitle">Enter a search term to analyze hundreds of tweets in seconds and view a visualization of recent public sentiment.</p>
-
                     <input type='text' id='searchBar' onChange={handleInputChange}></input>
-
-                    {/* <Link to={`search/${query}`}><button id='analyzeButton' onClick={(e) => handleSearch}>ANALYZE</button></Link> */}
                     <button id='analyzeButton' onClick={handleSearch}>ANALYZE</button>
                     {showMessage && <h4 id='validMessage'>Please Enter a Term To Analyze!</h4>}
-
                </div>
-               
 
           </div>
      )
